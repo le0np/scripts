cb2547aee3ec56d8e80a3a69c1622ef7a7a8adee
leon <77435396+le0np@users.noreply.github.com>
2024-11-06 15:18:16 +0100

    Update ssl_check.sh

--- pmp/ssl_check.sh
+++ pmp/ssl_check.sh
@@ -1,9 +1,16 @@
-#!/usr/bin/bash
-host=$1
+#!/bin/bash
 
-# Use curl to get SSL certificate details and extract relevant lines only
-curl --insecure -v https://$host 2>&1 | awk '
-    /^\*  subject:/ { print $0 }
-    /^\*  issuer:/ { print $0 }
-    /^\*  SSL certificate verify ok\./ { print $0; print ""; exit }
-'
+# Loop through each directory in /var/www/vhosts, excluding chroot and system directories
+for dir in $(find /var/www/vhosts -mindepth 1 -maxdepth 1 -type d ! -name 'chroot' ! -name 'system'); do
+    # Extract domain name from the directory path
+    domain=$(basename "$dir")
+
+    echo "Checking SSL certificate for $domain..."
+
+    # Run curl to check SSL certificate details
+    curl --insecure -v "https://$domain" 2>&1 | awk '
+        /^\*  subject:/ { print $0 }
+        /^\*  issuer:/ { print $0 }
+        /^\*  SSL certificate verify ok\./ { print $0; print ""; exit }
+    '
+done
